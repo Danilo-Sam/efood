a4c03c35edb5a4871cf382f7b237f4c49f60b2ce
DaniloSam <danilobrown17@gmail.com>
2024-06-05 18:21:14 -0300

    Realizando limpeza e organização dos arquivos

--- src/MenuList/components/List/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const ContainerList = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  gap: 32px;
-  max-width: 1024px;
-  margin: 0 auto;
-`
